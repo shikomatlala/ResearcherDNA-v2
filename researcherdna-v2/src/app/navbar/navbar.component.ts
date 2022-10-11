f8e4d38d2284e29292ff33b8d9076f72a0a595fa
shikmoatlala <shikomatlala@gmail.com>
2022-10-11 08:46:30 +0200

    Sidenav Assigned open links

--- researcherdna-v2/src/app/navbar/navbar.component.ts
+++ researcherdna-v2/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -11,6 +11,16 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  // @Output activePagff
+  pages = {home: 0, students: 1, project: 2, projects: 3, toolbox: 4, forum: 5, account: 6}
+  //The goal is to know what we are clicking, when we click a page the page needs to take us to the right location.
+  //Firstly the goal is to make sure that we can see anything that we are clicking.
 
+  //So we are going to have a single function that handles all the routes.
+  openPage(pageId:number){
+    console.log(pageId);
+    //Now we need a way to send this pageId to the main page.
+
+  }
   svgColor:string = "green";
 }
--- researcherdna-v2/src/app/toolbox/options/options.component.ts
+++ researcherdna-v2/src/app/toolbox/options/options.component.ts
@@ -7,14 +7,37 @@ import { OptionsService } from './options.service';
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
-
-  constructor(private optionsService: OptionsService) { }
   options:any;
+  constructor(private optionsService: OptionsService) { }
   ngOnInit(): void {
     console.log(this.optionsService.guidelines(1).subscribe((res)=>{
       console.log(res.guidelines);
       this.options = res.guidelines;
     }) )
   }
+
+  option2 = [ {id:"1", description:"INTRODUCTION"},
+
+  {id:"2", description:"BACKGROUND AND MOTIVATION/CONTEXTUALISATION"},
+
+  {id:"3", description:"PROBLEM STATEMENT"},
+
+  {id:"4", description:"RESEARCH QUESTIONS, AIM AND OBJECTIVES OF THE STUDY"},
+
+  {id:"5", description:"RESEARCH METHODOLOGY (could include discussion on measuring instruments)"},
+
+  {id:"6", description:"ETHICS"},
+
+  {id:"7", description:"TIME SCHEDULE"},
+
+  {id:"8", description:"BUDGET"},
+
+  {id:"9", description:"UPPORT STAFF"},
+
+  {id:"10", description:"Target PUBLICATIONS"},
+
+  {id:"11", description:"CHAPTER OUTLINE"},
+
+  {id:"12", description:"REFERENCES"}]
 
 }
